e8e8cabd032e15223f3dc93158173357773adc5e
Victor Cardona <vicardona7@gmail.com>
2021-10-27 19:34:35 +0200

    añadida la llamada al endpoint para iniciar sesion en el backend

--- auth/lib/services/google_signin_service.dart
+++ auth/lib/services/google_signin_service.dart
@@ -1,4 +1,5 @@
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSigninService {
   static final GoogleSignIn _googleSignIn = GoogleSignIn(
@@ -12,8 +13,19 @@ class GoogleSigninService {
       final account = await _googleSignIn.signIn();
       // generar el id token para enviar al backend
       final googleKey = await account?.authentication;
+      //print(googleKey?.idToken);
 
       // llamar servicio backend con el token
+      // en local no funciona la llamada ya que la conexion debe hacerse a google mediante https para la validacion
+      //final siginWithGoogleEndpoint =
+      //  Uri(scheme: 'https', host: '10.0.2.2:3000', path: '/google');
+      final session = await http.post(
+        Uri.parse('https://10.0.2.2:3000/google'),
+        body: {
+          'token': googleKey?.idToken,
+        },
+      );
+      // verificar que el estado es correcto, que se valido bien en el backedn
       return account;
     } catch (e) {
       return null;
